51fadb39f76f38266ca6ed4c37da963c07fe7fc7
rjshoemaker55 <rjshoemaker55@gmail.com>
2019-09-25 21:45:16 -0400

    got box check data system working

--- app.js
+++ app.js
@@ -1,38 +1,92 @@
-$('#error-display').hide();
-$('#winner-display').hide();
-$('#play-again').hide();
-
-
 // Starts event listener
 const start = () => {
+  for (i = 1; i < 10; i++) {
+    $('#box-' + i).data('checked', false).removeClass('red-box');
+  };
+  $('#play-again').hide();
+  $('#winner-display').hide();
+  $('#error-display').hide();
   $(document.body).on('click', '.boxes', checkBoxUser);
-}
+};
 
+// Check which boxes are checked already and assign them variables
+const boxCheck = () => ({
+  boxOneCheck: $('#box-1').data('checked'),
+  boxTwoCheck: $('#box-2').data('checked'),
+  boxThreeCheck: $('#box-3').data('checked'),
+  boxFourCheck: $('#box-4').data('checked'),
+  boxFiveCheck: $('#box-5').data('checked'),
+  boxSixCheck: $('#box-6').data('checked'),
+  boxSevenCheck: $('#box-7').data('checked'),
+  boxEightCheck: $('#box-8').data('checked'),
+  boxNineCheck: $('#box-9').data('checked')
+});
 
 // Checks if a square has already been taken, if not adds color and data-checked attribute
 const checkBoxUser = (e) => {
-  if ($(e.target).data('checked')) {
+  if ($(e.target).data('checked') !== false) {
+    console.log($(e.target).data('checked'))
     $('#error-display').show();
     $('#error-display').text('That box is already taken!');
     return;
   } else {
     $('#error-display').hide();
-    $(e.target).addClass('red-box').data('checked', true);
-    return checkWin();
+    $(e.target).addClass('red-box').data('checked', 'user');
+    return checkWinUser();
   };
 };
 
+const pickBoxComp = () => {
+  let boxOneRating = 0;
+  let boxTwoRating = 0;
+  let boxThreeRating = 0;
+  let boxFourRating = 0;
+  let boxFiveRating = 0;
+  let boxSixRating = 0;
+  let boxSevenRating = 0;
+  let boxEightRating = 0;
+  let boxNineRating = 0;
+};
+
+let {
+  boxOneCheck,
+  boxTwoCheck,
+  boxThreeCheck,
+  boxFourCheck,
+  boxFiveCheck,
+  boxSixCheck,
+  boxSevenCheck,
+  boxEightCheck,
+  boxNineCheck
+} = boxCheck();
+
+const boxArray = [
+  boxOneCheck,
+  boxTwoCheck,
+  boxThreeCheck,
+  boxFourCheck,
+  boxFiveCheck,
+  boxSixCheck,
+  boxSevenCheck,
+  boxEightCheck,
+  boxNineCheck
+];
+
+// const checkedBoxArray = boxArray.filter(box === )
+
 // Checks if the user has 3 boxes in a row
 const checkWinUser = () => {
-  let boxOneCheck = $('#box-1').data('checked');
-  let boxTwoCheck = $('#box-2').data('checked');
-  let boxThreeCheck = $('#box-3').data('checked');
-  let boxFourCheck = $('#box-4').data('checked');
-  let boxFiveCheck = $('#box-5').data('checked');
-  let boxSixCheck = $('#box-6').data('checked');
-  let boxSevenCheck = $('#box-7').data('checked');
-  let boxEightCheck = $('#box-8').data('checked');
-  let boxNineCheck = $('#box-9').data('checked');
+  let {
+    boxOneCheck,
+    boxTwoCheck,
+    boxThreeCheck,
+    boxFourCheck,
+    boxFiveCheck,
+    boxSixCheck,
+    boxSevenCheck,
+    boxEightCheck,
+    boxNineCheck
+  } = boxCheck();
 
   if (
   (boxOneCheck && boxTwoCheck && boxThreeCheck) ||
@@ -48,25 +102,17 @@ const checkWinUser = () => {
   };
 };
 
-
 // Displays win banner and shows the play again button.
 const winGame = () => {
   $('#winner-display').show();
-  $(document.body).off('click', '.boxes', chechBoxUser);
+  $(document.body).off('click', '.boxes', checkBoxUser);
   $('#play-again').show();
 };
 
 
 // Resets game board and turns on click listener back on
 $('#play-again').on('click', () => {
-  for (i = 1; i < 10; i++) {
-    $('#box-' + i).data('checked', false).removeClass('red-box');
-    const boxData = $('#box-' + i).data('checked');
-  };
-
-  $('#play-again').hide();
-  $('#winner-display').hide();
-  $(document.body).on('click', '.boxes', checkBoxUser);
+  start();
 });
 
 start();
